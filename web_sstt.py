893986e77602d64c1aa364e13b1ffff2da5c15a9
Rafa <rafael.guilleng@um.es>
2026-02-16 18:54:54 +0100

    todo

---
+++ web_sstt.py
@@ -0,0 +1,247 @@
+# coding=utf-8
+#!/usr/bin/env python3
+
+import socket
+import selectors    #https://docs.python.org/3/library/selectors.html
+import select
+import types        # Para definir el tipo de datos data
+import argparse     # Leer parametros de ejecución
+import os           # Obtener ruta y extension
+from datetime import datetime, timedelta # Fechas de los mensajes HTTP
+import time         # Timeout conexión
+import sys          # sys.exit
+import re           # Analizador sintáctico
+import logging      # Para imprimir logs
+
+
+BUFSIZE = 8192 # Tamaño máximo del buffer que se puede utilizar
+TIMEOUT_CONNECTION = 20 # Timout para la conexión persistente
+MAX_ACCESOS = 10
+
+# Extensiones admitidas (extension, name in HTTP)
+filetypes = {"gif":"image/gif", "jpg":"image/jpg", "jpeg":"image/jpeg", "png":"image/png", "htm":"text/htm", 
+             "html":"text/html", "css":"text/css", "js":"text/js"}
+
+# Configuración de logging
+logging.basicConfig(level=logging.INFO,
+                    format='[%(asctime)s.%(msecs)03d] [%(levelname)-7s] %(message)s',
+                    datefmt='%Y-%m-%d %H:%M:%S')
+logger = logging.getLogger()
+
+""" Esta función envía datos (data) a través del socket cs
+Devuelve el número de bytes enviados. """
+def enviar_mensaje(cs, data):
+    return cs.send(data.encode())
+
+""" Esta función recibe datos a través del socket cs
+Leemos la información que nos llega. recv() devuelve un string con los datos. """
+def recibir_mensaje(cs):
+    return cs.recv(BUFSIZE).decode()
+
+# Esta función cierra una conexión activa.
+def cerrar_conexion(cs):
+    cs.close()
+
+def procesar_error(cs, codigo):
+    if codigo == 400:
+        mensaje = "Bad Request"
+    elif codigo == 403:
+        mensaje = "Forbidden"
+    elif codigo == 404:
+        mensaje = "Not Found"
+    elif codigo == 405:
+        mensaje = "Method Not Allowed"
+    elif codigo == 505:
+        mensaje = "HTTP Version Not Supported"
+
+    direccion_imagen = str(codigo) + ".jpg" # Para la imagen del error
+    fecha_actual = datetime.now()
+    fecha_formateada = fecha_actual.strftime('%Y-%m-%d %H:%M:%S') # El formato del loggin    
+    html = "<html><body><h1>Error " + str(codigo) + ": " + mensaje + "</h1> <img src =\"" + direccion_imagen + "\"></body></html>"
+
+    respuesta = "HTTP/1.1 " + str(codigo) + " " + mensaje + "\r\n"
+    respuesta += "Date: " + fecha_formateada + "\r\n"
+    respuesta += "Server: Nombre (Ubuntu)\r\n"
+    respuesta += "Connection: close\r\n"
+    respuesta += "Content-Length: " + len(html) + "\r\n"
+    respuesta += "Content-Type: text/html; charset=utf-8\r\n"
+    respuesta += "\r\n"
+    respuesta += html
+
+    enviar_mensaje(cs, respuesta)
+
+# Esta función procesa la cookie cookie_counter
+def process_cookies(headers,  cs):
+    # 1. Se analizan las cabeceras en headers para buscar la cabecera Cookie
+    er_cookie = re.compile(r'Cookie')
+    er_cookie_counter = re.compile(r'(?P<nombre>cookie_counter)=(?P<valor>\d+)')
+    for header, valor_header in headers.items(): 
+        # 2. Una vez encontrada una cabecera Cookie se comprueba si el valor es cookie_counter
+        if er_cookie.fullmatch(header):
+            m = er_cookie_counter.search(valor_header)
+            if m:
+                valor = int(m.group('valor'))
+                # 4. Si se encuentra y tiene el valor MAX_ACCESSOS se devuelve MAX_ACCESOS
+                if valor >= MAX_ACCESOS:
+                    return MAX_ACCESOS
+                # 5. Si se encuentra y tiene un valor 1 <= x < MAX_ACCESOS se incrementa en 1 y se devuelve el valor
+                elif 1 <= valor < MAX_ACCESOS:
+                    valor += 1
+                    return valor
+    #3. Si no se encuentra cookie_counter , se devuelve 1
+    return 1
+
+""" Procesamiento principal de los mensajes recibidos.
+Típicamente se seguirá un procedimiento similar al siguiente (aunque el alumno puede modificarlo si lo desea) """
+def process_web_request(cs, webroot):
+    er_linea1 = re.compile(r'(?P<metodo>GET|POST|[A-Z]+)\s+(?P<ruta>\/\S*)\s+(?P<version>HTTP\/\d\.\d)')
+    er_cabecera = re.compile(r'(?P<header>.+):(?P<valor>.+)')
+    er_getopost = re.compile(r'GET|POST')
+    # * Bucle para esperar hasta que lleguen datos en la red a través del socket cs con select()
+    while True:
+        rsublist, wsublist, xsublist = select.select([cs], [], [], TIMEOUT_CONNECTION)
+        """ * Se comprueba si hay que cerrar la conexión por exceder TIMEOUT_CONNECTION segundos
+        sin recibir ningún mensaje o hay datos. Se utiliza select.select """
+        if not rsublist: # Si no hay leibles significa que saltó el timeout
+            break
+        # * Si no es por timeout y hay datos en el socket cs.
+        else:
+            # * Leer los datos con recv.
+            mensaje = recibir_mensaje(cs)
+            if not mensaje:
+                break # Para que el bucle se rompa cuando se cierra la conexion
+            lineas = mensaje.split('\r\n') # Dividir el string 
+            # * Analizar que la línea de solicitud y comprobar está bien formateada según HTTP 1.1
+            m = er_linea1.fullmatch(lineas[0])
+            if m:
+                # * Devuelve una lista con los atributos de las cabeceras.
+                # * Comprobar si la versión de HTTP es 1.1
+                if m.group('version') == "HTTP/1.1":
+                    # * Comprobar si es un método GET o POST, si no devolver Error 405 "Method Not Allowed". 
+                    if not er_getopost.fullmatch(m.group('metodo')):
+                        procesar_error(cs, 405)
+                    else:
+                        # * Leer URL y eliminar parámetros si los hubiera
+                        url = m.group('ruta')
+                        if '?' in url:
+                            url = url.split('?')[0] # Los parametros vienen despues del ?
+                        # * Comprobar si el recurso solicitado es /, En ese caso el recurso es index.html
+                        if url == '/':
+                            url = "index.html"
+                        # * Construir la ruta absoluta del recurso (webroot + recurso solicitado)
+                        ruta_absoluta = webroot+url
+                        # * Comprobar que el recurso (fichero) existe, si no devolver Error 404 "Not found"
+                        if not os.path.isfile(ruta_absoluta):
+                            procesar_error(cs, 404)
+                        """ * Analizar las cabeceras. Imprimir cada cabecera y su valor. Si la cabecera es Cookie comprobar
+                             el valor de cookie_counter para ver si ha llegado a MAX_ACCESOS. """
+                        cabeceras = {} # Dicccionario para las cabeceras
+                        for linea in lineas[1:]:
+                            if linea == "":
+                                break # Hemos llegado al fin de cabeceras
+                            m_cabecera = er_cabecera.fullmatch(linea)
+                            if m_cabecera:
+                                nombre = m_cabecera.group('header')
+                                valor_cabecera = m_cabecera.group('valor')
+                                cabeceras[nombre] = valor_cabecera
+                        contador_cookies = process_cookies(cabeceras, cs)
+                        #   Si se ha llegado a MAX_ACCESOS devolver un Error "403 Forbidden".
+                        if contador_cookies > MAX_ACCESOS:
+                            procesar_error(cs, 403)
+                        else:
+                            # * Obtener el tamaño del recurso en bytes.
+                            tamano = os.stat(ruta_absoluta).st_size
+                            # * Extraer extensión para obtener el tipo de archivo. Necesario para la cabecera Content-Type
+                            nombre_fichero = os.path.basename(ruta_absoluta)
+                            partes = nombre_fichero.split('.')
+                            if len(partes) > 1: # El fichero tiene un punto en el nombre
+                                extension = partes[-1] # La extensión es el úñtimo elemento de la lista
+                            else:
+                                extension = ""
+                            """ * Preparar respuesta con código 200. Construir una respuesta que incluya: la línea de respuesta y
+                            las cabeceras Date, Server, Connection, Set-Cookie (para la cookie cookie_counter),
+                            Content-Length y Content-Type. """
+                            if extension in filetypes: # Solo trata las extensiones válidas
+                                fecha_actual = datetime.now()
+                                fecha_formateada = fecha_actual.strftime('%Y-%m-%d %H:%M:%S') # El formato del loggin
+                                tipo_extension = filetypes[extension] # Extensión según el diccionario filetypes
+                                respuesta = "HTTP/1.1 200 OK\r\n"
+                                respuesta += "Date: " + fecha_formateada + "\r\n"
+                                respuesta += "Server: Nombre (Ubuntu)\r\n"
+                                respuesta += "Connection: timeout=" + str(TIMEOUT_CONNECTION) + ", max=100\r\n"
+                                respuesta += "Set-Cookie: cookie_counter=" + str(contador_cookies) + "\r\n"
+                                respuesta += "Content-Length: " + str(tamano) + "\r\n"
+                                respuesta += "Content-Type: " + tipo_extension + "; charset=utf-8\r\n"
+                                respuesta += "\r\n"
+                                # * Leer y enviar el contenido del fichero a retornar en el cuerpo de la respuesta.
+                                enviar_mensaje(cs, respuesta)
+                                # * Se abre el fichero en modo lectura y modo binario
+                                f = open(ruta_absoluta, 'rb')
+                                if tamano%BUFSIZE != 0:                       
+                                    bloques_leer = tamano//BUFSIZE + 1
+                                else:
+                                    bloques_leer = tamano//BUFSIZE
+                                for bloque in range(bloques_leer):
+                                    # * Se lee el fichero en bloques de BUFSIZE bytes (8KB)
+                                    datos = f.read(BUFSIZE)
+                                    cs.send(datos) # No usamos enviar_mensaje ya que no queremos codificar los datos
+                                    # * Cuando ya no hay más información para leer, se corta el bucle                        
+                                f.close()
+                            else:
+                                # Error "Not Found"
+                                procesar_error(cs, 404)
+                else:
+                    # Error "HTTP Version Not Supported"
+                    procesar_error(cs, 505)
+            else:
+                # Error "Bad Request"
+                procesar_error(cs, 400)
+        # * Si es por timeout, se cierra el socket tras el período de persistencia.
+        # * NOTA: Si hay algún error, enviar una respuesta de error con una pequeña página HTML que informe del error.
+
+
+def main():
+    """ Función principal del servidor
+    """
+    try:
+        # Argument parser para obtener la ip y puerto de los parámetros de ejecución del programa. IP por defecto 0.0.0.0
+        parser = argparse.ArgumentParser()
+        parser.add_argument("-p", "--port", help="Puerto del servidor", type=int, required=True)
+        parser.add_argument("-ip", "--host", help="Dirección IP del servidor o localhost", required=True)
+        parser.add_argument("-wb", "--webroot", help="Directorio base desde donde se sirven los ficheros (p.ej. /home/user/mi_web)")
+        parser.add_argument('--verbose', '-v', action='store_true', help='Incluir mensajes de depuración en la salida')
+        args = parser.parse_args()
+
+        if args.verbose:
+            logger.setLevel(logging.DEBUG)
+
+        logger.info('Enabling server in address {} and port {}.'.format(args.host, args.port))
+
+        logger.info("Serving files from {}".format(args.webroot))
+
+        # * Crea un socket TCP (SOCK_STREAM)
+        server = socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM, proto=0)
+        #* Permite reusar la misma dirección previamente vinculada a otro proceso. Debe ir antes de sock.bind
+        server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        #* Vinculamos el socket a una IP y puerto elegidos
+        server.bind((args.host, args.port))
+        #* Escucha conexiones entrantes
+        server.listen()
+        #* Bucle infinito para mantener el servidor activo indefinidamente
+        while True:
+            # - Aceptamos la conexión
+            conn, addr = server.accept()
+            # - Creamos un proceso hijo
+            pid = os.fork()
+            # - Si es el proceso hijo se cierra el socket del padre y procesar la petición con process_web_request()
+            if pid == 0:
+                cerrar_conexion(server)
+                process_web_request(conn, args.webroot) 
+            # - Si es el proceso padre cerrar el socket que gestiona el hijo.
+            else:
+                cerrar_conexion(conn)
+    except KeyboardInterrupt:
+        True
+
+if __name__== "__main__":
+    main()
